3d56fa7f6a6ed507d8f448aea0b4032544d9ff4b
ShunpoMechanics <jlc3939@gmail.com>
2018-11-23 15:11:57 -0500

    Added helper functions to FileManager and Neighbor as well as added indicators for statuses

--- StartRemotePeers/Neighbor.java
+++ StartRemotePeers/Neighbor.java
@@ -14,9 +14,61 @@ public class Neighbor {
     String hostname;
     int listeningPort;
     int hasFile;
-    
+	boolean isChoked;
+	boolean isPreferred;
+	boolean isInterrested;
+	
     int downloadRate;
+	
+	public void choke()
+	{this.isChoked = true;}
+	
+	public void unchoke()
+	{this.isChoked = false;}
 
+	public void prefer()
+	{this.isPreferred = true;}
+	
+	public void notPrefer()
+	{this.isPreferred = false;}
+	
+	public void interested()
+	{this.isInterrested = true;}
+	
+	public void disinterested()
+	{this.isInterrested = false;}
+	
+	public boolean getPreferredStatus()
+	{return isPreferred;}
+	
+	public boolean getInterestStatus()
+	{return isInterrested;}
+	
+	public boolean getChokeStatus()
+	{return isChoked;}
+	
+	public int getStatus()
+	{
+		int choke = 0;
+		int interrested = 0;
+		int preferreded = 0;
+		int statusCode = 0;
+		if(isChoked)
+			choke = 1;
+		if(isInterrested)
+			interrested = 1;
+		if(isPreferred)
+			preferreded = 1;
+		choke = choke*100;
+		interrested = interrested*10;
+		statusCode = choke + interrested + preferreded;
+		return statusCode;
+		//Returns a status code with all 3 fields in order to minimize function calls
+		//Ex: if unchoked, uninterrested, but preferreded the statusCode returns 001
+		//If choked, interrested and preferreded it returns 111
+		//Define behavior with a switch using the various combincations instead of if(true && true || false) etc
+	}
+	
     /**
      * @return the id
      */
